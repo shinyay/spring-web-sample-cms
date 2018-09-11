8c2528227c521c5266720539dca8f4c677aa414e
shinyay <shinya.com@gmail.com>
2018-09-11 21:33:36 +0900

    Modify News model class for adding new methods

--- src/main/java/io/pivotal/syanagihara/web/cms/domain/models/News.java
+++ src/main/java/io/pivotal/syanagihara/web/cms/domain/models/News.java
@@ -1,16 +1,30 @@
 package io.pivotal.syanagihara.web.cms.domain.models;
 
+import lombok.Data;
+
+import java.util.HashSet;
 import java.util.Set;
 
+@Data
 public class News {
 
     String id;
     String title;
     String content;
     User author;
-    Set<User> mandatoryReviewers;
-    Set<Review> reviewers;
-    Set<Category> categories;
-    Set<Tag> tags;
+    Set<User> mandatoryReviewers = new HashSet<>();
+    Set<Review> reviewers = new HashSet<>();
+    Set<Category> categories = new HashSet<>();
+    Set<Tag> tags = new HashSet<>();
 
+    public Review review(String userId,String status){
+        final Review review = new Review(userId, status);
+        this.reviewers.add(review);
+        return review;
+    }
+
+    public Boolean revised() {
+        return this.mandatoryReviewers.stream().allMatch(reviewer -> this.reviewers.stream()
+                .anyMatch(review -> reviewer.id.equals(review.userId) && "approved".equals(review.status)));
+    }
 }
