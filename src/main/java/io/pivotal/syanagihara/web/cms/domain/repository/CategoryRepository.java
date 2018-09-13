d6c1a7fa3898e3c24962d96485790e8e9d96fd69
shinyay <shinya.com@gmail.com>
2018-09-13 16:41:50 +0900

    Add Repostory for Category and User

---
+++ src/main/java/io/pivotal/syanagihara/web/cms/domain/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package io.pivotal.syanagihara.web.cms.domain.repository;
+
+import io.pivotal.syanagihara.web.cms.domain.models.Category;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryRepository extends AbstractRepository<Category> {
+}
---
+++ src/main/java/io/pivotal/syanagihara/web/cms/domain/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package io.pivotal.syanagihara.web.cms.domain.repository;
+
+import io.pivotal.syanagihara.web.cms.domain.models.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserRepository extends AbstractRepository<User> {
+}
