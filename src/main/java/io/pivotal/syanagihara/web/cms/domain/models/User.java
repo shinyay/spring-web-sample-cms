623b8e5a8ca3462c758a116bee2993c0aa6bb7c3
shinyay <shinya.com@gmail.com>
2018-09-18 20:24:53 +0900

    Add Entity and Id Annotation for JPA

--- src/main/java/io/pivotal/syanagihara/web/cms/domain/models/User.java
+++ src/main/java/io/pivotal/syanagihara/web/cms/domain/models/User.java
@@ -1,10 +1,21 @@
 package io.pivotal.syanagihara.web.cms.domain.models;
 
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
+@Entity
+@Table(name = "system_user")
 public class User {
 
+    @Id
+    @GeneratedValue(generator = "system=-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
     String id;
     String identity;
     String name;
