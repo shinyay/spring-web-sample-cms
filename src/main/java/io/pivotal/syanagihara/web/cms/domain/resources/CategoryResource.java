b4ad2ab251a8ea95f920cec5476f6a0f359a867c
shinyay <shinya.com@gmail.com>
2018-09-18 18:25:17 +0900

    Add first api document

--- src/main/java/io/pivotal/syanagihara/web/cms/domain/resources/CategoryResource.java
+++ src/main/java/io/pivotal/syanagihara/web/cms/domain/resources/CategoryResource.java
@@ -2,6 +2,10 @@ package io.pivotal.syanagihara.web.cms.domain.resources;
 
 import io.pivotal.syanagihara.web.cms.domain.models.Category;
 import io.pivotal.syanagihara.web.cms.domain.vo.CategoryRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,9 +15,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/category")
+@Api(tags = "category", description = "Category API")
 public class CategoryResource {
 
     @GetMapping(value = "/{id}")
+    @ApiOperation(value = "Find category", notes = "Find the Category by ID")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Categories found"),
+            @ApiResponse(code = 404, message = "Category not found")
+    })
     public ResponseEntity<Category> findOne(@PathVariable("id") String id){
 
         return ResponseEntity.ok(new Category());
