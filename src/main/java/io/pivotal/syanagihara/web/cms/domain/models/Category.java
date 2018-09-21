0ac764a80142068936470301a15bba2244e3f431
shinyay <shinya.com@gmail.com>
2018-09-21 10:55:20 +0900

    Modify annotation for using mongodb

--- src/main/java/io/pivotal/syanagihara/web/cms/domain/models/Category.java
+++ src/main/java/io/pivotal/syanagihara/web/cms/domain/models/Category.java
@@ -1,21 +1,15 @@
 package io.pivotal.syanagihara.web.cms.domain.models;
 
 import lombok.Data;
-import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Data
-@Entity
-@Table(name = "category")
+@Document(collection = "category")
 public class Category {
 
     @Id
-    @GeneratedValue(generator = "system-uuid")
-    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
     String id;
 
     String name;
